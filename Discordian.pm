6d03d3218b56d460a2563063ec3558a984ce6021
Rich Bowen <rbowen@rcbowen.com>
2000-08-13 19:59:34 +0000

    Import of RBOW/Date-Discordian-1.01 from CPAN.
    gitpan-cpan-distribution: Date-Discordian
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         RBOW/Date-Discordian-1.01.tar.gz
    gitpan-cpan-author:       RBOW
    gitpan-cpan-maturity:     released

--- Discordian.pm
+++ Discordian.pm
@@ -5,24 +5,48 @@ require Exporter;
 
 @ISA = qw(Exporter AutoLoader);
 @EXPORT = qw( discordian );
-@EXPORT_OK = qw( @SEASONS @DAYS );
-$VERSION = '1.00';
+@EXPORT_OK = qw( @SEASONS @DAYS isleap);
+$VERSION = '1.01';
 
 @SEASONS = qw(Chaos Discord Confusion Bureaucracy Aftermath);
 @DAYS = ('Sweetmorn', 'Boomtime', 'Pungenday', 'Prickle Prickle', 'Setting Orange');
 
 sub discordian	{
-	my ($date) = @_;
-	my ($yday, $season, $day, $dow, $datestring);
+	my ($datetime) = @_;
+	# $datetime ||= time;
+	my ($year, $yday, $season, $day, $dow, $datestring);
 	
-	$yday = (localtime($date))[7];
-	($season, $day);
+	($year, $yday) = (localtime($datetime))[5, 7];
+	$yday++; # yday is 0-based;
+
+	if (isleap($year))	{
+		if ($yday <= 59)	{
+			# nothing changes
+		}
+		elsif ($yday == 60)	{
+			# leap day!
+			return "St. Tib's Day";
+		}
+		else {
+			# The rest of the year after leap day
+			$yday--;
+		}
+	} # End leap year stuff
+
 	$season = int($yday/73);
-	$day = $yday % 73;
+	$day = ($yday % 73) || 73;
 	$dow = $yday % 5;
 
 	$datestring = $DAYS[$dow-1] . ', ' . $SEASONS[$season] . ' ' . $day;
-	return wantarray ? ($season, $day, $dow) : $datestring ;
+	return $datestring ;
+}
+
+sub isleap	{
+	my ($year) = @_;
+	$year += 1900;
+	return 1 if $year%4==0;
+	return 1 if ($year%4==0 && !$year%100);
+	return 0;
 }
 
 1;
@@ -37,27 +61,21 @@ Date::Discordian - Calculate the Discordian date of a particular day
 
   use Date::Discordian;
   $discordian = discordian(time);
-  ($season, $day, $day_of_week) = discordian(time);
 
 =head1 DESCRIPTION
 
 Calculate the Discordian date of a particular 'real' date.
 
 Date::Discordian exports just one function - discordian() - which,
-when given a time value, returns either a list of the Discordian
-season, day of that season, and the day of the week, or a single string
-of those values, depending on context.
+when given a time value, returns a string, giving the Discordian
+date for the given day.
 
 I'm really not sure how this would ever be used, so if you actually use
 this, send me a note.
 
 =head1 Bugs/To Do
 
-Does not handle leap years at all. In fact, it's really not working
-correctly now. It's off by a day. Sort of. It appears to work, because
-2000 is a leap year.
-
-Also, I'd like for it to be able to return the various Holydays in 
+I'd like for it to be able to return the various Holydays in 
 some useful fashion, ala ddate.
 
 =head1 AUTHOR
--- test.pl
+++ test.pl
@@ -6,7 +6,7 @@
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-BEGIN { $| = 1; print "1..2\n"; }
+BEGIN { $| = 1; print "1..5\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Date::Discordian;
 $loaded = 1;
@@ -18,11 +18,39 @@ print "ok 1\n";
 # (correspondingly "not ok 13") depending on the success of chunk 13
 # of the test code):
 
-$discordian = discordian(966136897);
-if ($discordian eq 'Prickle Prickle, Bureaucracy 5')	{
+print "January 1, 2000 ... ";
+$discordian = discordian(946702800);
+if ($discordian eq 'Sweetmorn, Chaos 1')	{
 	print "ok 2\n";
 }
 else {
 	print "not ok 2\n";
+}
+
+print "February 28, 2000 ... ";
+$discordian = discordian(951714000);
+if ($discordian eq 'Prickle Prickle, Chaos 59')	{
+	print "ok 3\n";
+}
+else {
+	print "not ok 3\n";
+}
+
+print "February 29, 2000 ... ";
+$discordian = discordian(951800400);
+if ($discordian eq 'St. Tib\'s Day')	{
+	print "ok 4\n";
+}
+else {
+	print "not ok 4\n";
+}
+
+print "August 12, 2000 ... ";
+$discordian = discordian(966108845);
+if ($discordian eq 'Prickle Prickle, Bureaucracy 5')	{
+	print "ok 5\n";
+}
+else {
+	print "not ok 5\n";
 }
 
